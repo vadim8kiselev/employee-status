97042e99412ad02bd4758fc4802096928d0e927d
Vadim Kiselev <vadim8kiselev@users.noreply.github.com>
2016-07-08 02:53:40 +0300

    hot fix: add home path to stop script

--- daemon.py
+++ daemon.py
@@ -1,14 +1,15 @@
 import subprocess
 import sys, os
-
+import time
 
 def daemon(PYTHON_HOME, SCRIPT_HOME):    
     pid = subprocess.Popen('{0}\python.exe {1}\server.py'.format(PYTHON_HOME, SCRIPT_HOME), creationflags=0x00000008).pid
-    generate_stop_script(pid)
+    generate_stop_script(SCRIPT_HOME, pid)
 
 
-def generate_stop_script(pid):
-    with open('stop_daemon.py', 'w') as script:
+def generate_stop_script(HOME, pid):
+    with open('{0}\stop_daemon.py'.format(HOME), 'w') as script:
+	print 'generate'
         script.write('import sys, os' + '\n'
                    + 'os.system("taskkill /pid {0} /f")'.format(str(pid)) + '\n'
                    + 'os.remove(sys.argv[0])')
@@ -16,9 +17,10 @@ def generate_stop_script(pid):
 def main():
     PYTHON_HOME = 'C:\Python27'
     SCRIPT_HOME = os.path.dirname(sys.argv[0])
-    if os.path.exists(PYTHON_HOME) and not os.path.exists('stop_daemon.py'):
+    
+    if os.path.exists(PYTHON_HOME) and not os.path.exists('{0}\stop_daemon.py'.format(SCRIPT_HOME)):
         daemon(PYTHON_HOME, SCRIPT_HOME)
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__': 
     main()
