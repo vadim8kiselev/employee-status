343cf9e505b7655d06b97eb20dab6bd1be07f302
Vadim Kiselev <vadim8kiselev@users.noreply.github.com>
2016-07-10 17:19:47 +0300

    Add cleanup of index.html

--- daemon.py
+++ daemon.py
@@ -11,7 +11,8 @@ def generate_stop_script(HOME, pid):
     with open('{0}\stop_daemon.py'.format(HOME), 'w') as script:
         script.write('import sys, os' + '\n'
                    + 'os.system("taskkill /pid {0} /f")'.format(str(pid)) + '\n'
-                   + 'os.remove(sys.argv[0])')
+                   + 'os.remove(sys.argv[0])' + '\n'
+                   + 'os.remove("index.html")')
 
 def main():
     PYTHON_HOME = sys.exec_prefix
@@ -21,5 +22,5 @@ def main():
         daemon(PYTHON_HOME, SCRIPT_HOME)
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
